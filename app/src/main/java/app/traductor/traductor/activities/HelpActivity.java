77a14e42867f8dc61d38d67eb57d127412526304
AbrahamLCodes <60500103+AbrahamLCodes@users.noreply.github.com>
2020-12-02 23:36:01 -0700

    Video agregado

--- app/src/main/java/app/traductor/traductor/activities/HelpActivity.java
+++ app/src/main/java/app/traductor/traductor/activities/HelpActivity.java
@@ -2,15 +2,27 @@ package app.traductor.traductor.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import app.traductor.traductor.R;
 
 public class HelpActivity extends AppCompatActivity {
 
     private ImageView regresar;
+    private TextView video;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +30,32 @@ public class HelpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_help);
 
         regresar = findViewById(R.id.regresar);
+        video = findViewById(R.id.video);
+
         regresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onBackPressed();
             }
         });
+        video.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isNetworkAvailable()){
+                    String url = "https://www.youtube.com/watch?v=rLL4LJdPRtY&feature=youtu.be";
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url));
+                    startActivity(i);
+                }
+            }
+        });
     }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
 }
